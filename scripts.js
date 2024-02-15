fcf918d761e0aeb53c5f3ca11b9f36dab3b90a62
José Luis Reig Guerrero <joseluis.reig@alumnos.upm.es>
2024-02-15 12:43:01 +0100

    fichero scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("ejemplo").innerHTML = "Las escuderías que competirán por el mundial de F1 este 2024 serán:";
+
+      const coches = ["Red Bull", " Aston Martin", " Ferrari"];
+      coches.push(" McLaren");
+      coches[4] = " Mercedes";
+      coches.push(" Alpine", " Alpha Tauri", " Sauber", " Williams", " Haas");
+      document.getElementById("cars").innerHTML = coches;
